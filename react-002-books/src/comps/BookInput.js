64af1dec428f088b647791c75fa53638d64a10dd
PollyGotACracker <bitterns96@gmail.com>
2022-12-15 11:13:00 +0900

    naverBook 도서정보

---
+++ react-002-books/src/comps/BookInput.js
@@ -0,0 +1,12 @@
+/**
+ * 함수형 컴포넌트
+ */
+const BookInput = (props) => {
+  return (
+    <div className="container">
+      <input name="b_title" placeholder="도서명 입력 후 Enter..." />
+    </div>
+  );
+};
+
+export default BookInput;
