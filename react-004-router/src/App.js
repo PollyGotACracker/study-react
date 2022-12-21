f61af0b8014fb6c52321df1f0e295c39b50dff7e
PollyGotACracker <bitterns96@gmail.com>
2022-12-21 16:37:46 +0900

    react-router-dom

--- react-004-router/src/App.js
+++ react-004-router/src/App.js
@@ -1,7 +1,7 @@
 import logo from "./logo.svg";
 import "./css/App.css";
 import Nav from "./page/Nav";
-import MainRouter from "./page/MainRrouter";
+import MainRouter from "./page/MainRouter";
 import { BrowserRouter } from "react-router-dom";
 
 function App() {
